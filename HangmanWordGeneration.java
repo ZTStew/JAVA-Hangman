d240b1b8bf1a6651fc03740550500b02d910ee6f
ZTStew <ZT.Stewart.5@gmail.com>
2023-02-17 09:32:08 -0500

    Started adding word selection

--- HangmanWordGeneration.java
+++ HangmanWordGeneration.java
@@ -1,7 +1,23 @@
 /*
  * Class handles selecting word that will be guessed
  */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class HangmanWordGeneration {
+  String word;
 
+  Scanner scan = new Scanner(System.in);
+
+  public HangmanWordGeneration() {
+
+  }
+
+  public String setWord(String wordLength) {
+    
+    File file = new File("len_" + wordLength + "_words.txt");
+    
+    return "";
+  }
 }
