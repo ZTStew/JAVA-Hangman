e52571114e2dc24cff83eca688a172246e917999
ZTStew <ZT.Stewart.5@gmail.com>
2023-02-17 13:41:15 -0500

    Pre file search refactor

--- HangmanWordGeneration.java
+++ HangmanWordGeneration.java
@@ -8,15 +8,26 @@ import java.util.Scanner;
 public class HangmanWordGeneration {
   String word;
 
-  Scanner scan = new Scanner(System.in);
+  // Scanner scan = new Scanner(System.in);
 
   public HangmanWordGeneration() {
 
   }
 
   public String setWord(String wordLength) {
-    
-    File file = new File("len_" + wordLength + "_words.txt");
+    try {
+      File file = new File("len_" + wordLength + "_words.txt");
+      Scanner scan = new Scanner(file);
+
+      while (scan.hasNextLine()) {
+        String data = scan.nextLine();
+
+        System.out.println(data);
+      }
+      scan.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("ERROR: " + e);
+    }
     
     return "";
   }
