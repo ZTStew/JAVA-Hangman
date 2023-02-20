abd5b66403bd314504ddbfac17fd067240121fa5
ZTStew <ZT.Stewart.5@gmail.com>
2023-02-20 13:57:15 -0500

    Added replay option. Need to add resets to all settings

--- HangmanTest.java
+++ HangmanTest.java
@@ -11,68 +11,68 @@ public class HangmanTest {
     Scanner scan = new Scanner(System.in);
 
     ArrayList<String> guesses = new ArrayList<String>();
-    Boolean game_loop = true;
+    Boolean gameLoop = true;
     // int strikes = 1;
     int correct = 0;
-    boolean playAgain = false;
+    boolean playAgain = true;
 
 
+    while (playAgain) {
+      /*
+       * Difficulty selection
+       */
+      HangmanSettingsSelect settings = new HangmanSettingsSelect();
+      HangmanGameFunctions gameFunction = new HangmanGameFunctions(settings);
+      settings.setDifficulty();
+      settings.setWordLength();
+      settings.setWord();
 
-    /*
-    * Difficulty selection
-    */
-    HangmanSettingsSelect settings = new HangmanSettingsSelect();
-    HangmanGameFunctions gameFunction = new HangmanGameFunctions(settings);
-    settings.setDifficulty();
-    settings.setWordLength();
-    settings.setWord();
-
-    if (settings.getDifficulty() == 0) {
-      scan.close();
-      return;
-    }
-
-
-    /*
-     * End Difficulty Selection
-     */
-
-    // generates word that will be guessed
-    // String word = words[random.nextInt(10)];
-    System.out.println("Word To Guess: " + settings.getWord());
-
-
-    while (game_loop) {
-      if (correct >= settings.getWord().length()) {
-        System.out.println("Game Over! Word, " + settings.getWord().toUpperCase() + ", Correctly Guessed!");
-
-        /* Will prompt player if they want to play again */
-        // System.out.print("Play Again? (y/n)");
-        // String replay = scan.nextLine();
-        // if (replay.charAt(0) == 'y') {
-        //   playAgain = true;
-        // }
-
-
-        game_loop = false;
-
-      // Kills game after number of strikes equals 0
-      } else if (settings.getStrikes() > 0) {
-        // Sets up encoded word
-        gameFunction.setEncodedWord(settings.getWord(), guesses);
-
-        System.out.println("Word: " + gameFunction.getEncodedWord());
-        System.out.println("Strikes Remaining: " + settings.getStrikes());
-
-        correct = gameFunction.getUserGuess(settings.getWord(), guesses, correct);
-
-      } else {
-        System.out.println("Game Over! Word, " + settings.getWord().toUpperCase() + ", Was Not Guessed!");
-
-        game_loop = false;
+      if (settings.getDifficulty() == 0) {
+        scan.close();
+        return;
       }
-    }
 
+      System.out.println("Word To Guess: " + settings.getWord());
+
+      playAgain = false;
+      gameLoop = true;
+
+      while (gameLoop) {
+        if (correct >= settings.getWord().length()) {
+          System.out.println("Game Over! Word, " + settings.getWord().toUpperCase() + ", Correctly Guessed!");
+
+          /* Will prompt player if they want to play again */
+          System.out.print("Would You Like To Play Again? (y/n)");
+          String replay = scan.nextLine();
+          if (replay.charAt(0) == 'y') {
+            playAgain = true;
+          }
+
+          gameLoop = false;
+
+        // Kills game after number of strikes equals 0
+        } else if (settings.getStrikes() > 0) {
+          // Sets up encoded word
+          gameFunction.setEncodedWord(settings.getWord(), guesses);
+
+          System.out.println("Word: " + gameFunction.getEncodedWord());
+          System.out.println("Strikes Remaining: " + settings.getStrikes());
+
+          correct = gameFunction.getUserGuess(settings.getWord(), guesses, correct);
+
+        } else {
+          System.out.println("Game Over! Word, " + settings.getWord().toUpperCase() + ", Was Not Guessed!");
+
+          System.out.print("Would You Like To Play Again? (y/n)");
+          String replay = scan.nextLine();
+          if (replay.charAt(0) == 'y') {
+            playAgain = true;
+          }
+
+          gameLoop = false;
+        }
+      } // while(gameLoop)
+    } // while(playAgain)
     scan.close();
-  }
-}
+  } // main
+} // HangmanTest
